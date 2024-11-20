e839d8a2f4e1d02ff04a045e3049e689270377ef
Andrew5513 <andrewlim389@gmail.com>
2024-11-20 17:30:49 +0700

    Update For REsult Test
    Update For REsult Test

--- Assets/MainScenes/Script/GameResult/FInalResult/FinalResultScreen.cs
+++ Assets/MainScenes/Script/GameResult/FInalResult/FinalResultScreen.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Tambahkan ini untuk menggunakan komponen Image
+
 
 public class FinalResultScreen : MonoBehaviour
 {
     public TMP_Text finalPlayer1ScoreText;
     public TMP_Text finalPlayer2ScoreText;
     public TMP_Text winnerText;
+
+    // Tambahkan variabel untuk gambar kemenangan
+    public Image winnerImage;           // Image UI component untuk menampilkan gambar
+    public Sprite blueTeamWinSprite;    // Gambar A (untuk Tim Biru menang)
+    public Sprite redTeamWinSprite;     // Gambar B (untuk Tim Merah menang)
+    public Sprite drawSprite;           // Gambar C (untuk kondisi Draw)
+
 
     private void Start()
     {
@@ -20,10 +29,17 @@ public class FinalResultScreen : MonoBehaviour
         if (GameRumbleGameManagerForScore.instance.player1Wins > GameRumbleGameManagerForScore.instance.player2Wins)
         {
             winnerText.text = "Tim Biru Wins!";
+            winnerImage.sprite = blueTeamWinSprite;  // Tampilkan gambar A jika Tim Biru menang
         }
-        else if (GameRumbleGameManagerForScore.instance.player2Wins > GameRumbleGameManagerForScore.instance.player1Wins)
+        else if (GameRumbleGameManagerForScore.instance.player1Wins < GameRumbleGameManagerForScore.instance.player2Wins)
         {
             winnerText.text = "Tim Merah Wins!";
+            winnerImage.sprite = redTeamWinSprite;   // Tampilkan gambar B jika Tim Merah menang
+        }
+        else
+        {
+            winnerText.text = "Draw!";               // Jika skor seri
+            winnerImage.sprite = drawSprite;         // Tampilkan gambar Draw
         }
     }
 
